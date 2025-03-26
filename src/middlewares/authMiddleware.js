bfacac16e2185303c6316acee9d3883886f19cec
AbdullahNazir0 <Abdullah.nazir289@gmail.com>
2025-03-26 18:11:10 +0500

    Fixed middleware bug

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,10 +2,7 @@ import api from '../utils/axios.js'
 
 export async function authMiddleware(to, from, next) {
   try {
-    if (!authStore.isAuthenticated) {
-      const response = await api.get('/users/status', { withCredentials: true })
-      authStore.setUser(response.data)
-    }
+    await api.get('/users/status', { withCredentials: true })
 
     next()
   } catch (error) {
