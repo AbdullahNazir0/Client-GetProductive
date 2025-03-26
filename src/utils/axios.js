e025aca0b4fa6dcea815ee2052053f969940a70e
AbdullahNazir0 <Abdullah.nazir289@gmail.com>
2025-03-26 15:12:13 +0500

    Fixed issues x2

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.BASE_URI,
+  baseURL: import.meta.env.VITE_BASE_URI,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
